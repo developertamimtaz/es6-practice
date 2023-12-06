8fd566444f1e52d92df22017797c72a84ea45c03
developertamimtaz <farhanahmedtaz21@gmail.com>
2023-12-06 22:54:51 +0600

    letconst done

---
+++ letconst.js
@@ -0,0 +1,27 @@
+const hubby = "Elias Kanchon";
+// can't change value of const variable. hubby = "Omar Sunny"
+console.log(hubby);
+
+
+const numbers = [12, 45];
+//can changes array elements.
+numbers[1] = 88;
+//can add elements in array.
+numbers.push(12);
+//can changes overall array.
+//numbers = ["Sunny", "Salman", "Omit Hasan"];
+const nayok = {name: "Sakib Khan", phone: 4578};
+
+//console.log(numbers);
+
+let patientName = "Rahim Chacha";
+//Let can always change value of variable.
+patientName = "Fatema Khala";
+console.log(patientName);  
+
+//Let is a liked proved
+let sum = 0;
+for(var i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
