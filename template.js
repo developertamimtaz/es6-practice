724d308393e26d04a43ca3180039f0d0f3ac1fc5
developertamimtaz <farhanahmedtaz21@gmail.com>
2023-12-07 23:01:41 +0600

    template javascript

---
+++ template.js
@@ -0,0 +1,22 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + "is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+
+//Template Old
+/* const multiLine = "I love you\n"
+    + "I miss you\n"
+    + "I need you";
+
+    console.log(multiLine); */
+
+//Template ES6 New
+const multiLine2 = `I love you
+i miss you
+no. I don't need you
+Baily road e dorkar nai.` 
+
+console.log(multiLine2);
+
+    
+//console.log(fullName2);
